d8a2060bee6a55c4b8bf937ad96feed10227373b
Aram Harrow <harrow@gmail.com>
2020-05-03 21:24:55 -0400

    metropolis

--- 3_monte_carlo/metropolis.py
+++ 3_monte_carlo/metropolis.py
@@ -1,44 +1,47 @@
+# Adapted by Aram Harrow from course material for [Computational Methods in Many-Body Physics](https://github.com/jhauschild/lecture_comp_methods) by Prof. Frank Pollmann, Prof. Michael Knap and Johannes Hauschild.
+
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import time
 
 from numba import jit
 
-
-@jit(nopython=True)
-def energy(system, i, j, L):
-    """Energy function of spins connected to site (i, j)."""
-    return -1. * system[i, j] * (system[np.mod(i - 1, L), j] + system[np.mod(i + 1, L), j] +
-                                 system[i, np.mod(j - 1, L)] + system[i, np.mod(j + 1, L)])
-
-
 @jit
-def prepare_system(L):
+def prepare_system(Lx, Ly):
     """Initialize the system."""
-    system = 2 * (0.5 - np.random.randint(0, 2, size=[L, L]))
+    system = np.zeros((Lx,Ly))
+    for i in range(Lx):
+        for j in range(Ly):
+            system[i,j] = 1 - 2 * np.random.randint(2)
     return system
 
 @jit(nopython=True)
-def measure_energy(system):
-    L = system.shape[0]
-    E = 0
-    for i in range(L):
-        for j in range(L):
-            E += energy(system, i, j, L) / 2.
-    return E
+def site_energy(system, i, j, Lx, Ly):
+    """Energy function of spins connected to site (i, j)."""
+    return -1. * system[i, j] * (system[np.mod(i - 1, Lx), j] + system[np.mod(i + 1, Lx), j] +
+                                 system[i, np.mod(j - 1, Ly)] + system[i, np.mod(j + 1, Ly)])
 
+@jit(nopython=True)
+def total_energy(system):
+    (Lx,Ly) = system.shape
+    E = 0
+    for i in range(Lx):
+        for j in range(Ly):
+            E += site_energy(system, i, j, Lx, Ly) / 2.
+    return E
 
 @jit(nopython=True)
 def metropolis_loop(system, T, N_sweeps, N_eq, N_flips):
     """ Main loop doing the Metropolis algorithm."""
-    E = measure_energy(system)
-    L = system.shape[0]
+    E = total_energy(system)
+    (Lx, Ly) = system.shape
     E_list = []
     for step in range(N_sweeps + N_eq):
-        i = np.random.randint(0, L)
-        j = np.random.randint(0, L)
+        i = np.random.randint(0, Lx)
+        j = np.random.randint(0, Ly)
 
-        dE = -2. * energy(system, i, j, L)
+        dE = -2. * site_energy(system, i, j, Lx, Ly)
         if dE <= 0.:
             system[i, j] *= -1
             E += dE
@@ -49,24 +52,23 @@ def metropolis_loop(system, T, N_sweeps, N_eq, N_flips):
         if step >= N_eq and np.mod(step, N_flips) == 0:
             # measurement
             E_list.append(E)
+
     return np.array(E_list)
-
-
 
 
 if __name__ == "__main__":
     """ Scan through some temperatures """
     # Set parameters here
-    L = 4  # Linear system size
-    N_sweeps = 5000  # Number of steps for the measurements
-    N_eq = 1000  # Number of equilibration steps before the measurements start
+    L = 20  # Linear system size
+    N_sweeps = 10000  # Number of steps for the measurements
+    N_eq = 5000  # Number of equilibration steps before the measurements start
     N_flips = 10  # Number of steps between measurements
-    N_bins = 10  # Number of bins use for the error analysis
+    N_bins = 20  # Number of bins use for the error analysis
 
     T_range = np.arange(1.5, 3.1, 0.1)
 
     C_list = []
-    system = prepare_system(L)
+    system = prepare_system(L, L)
     for T in T_range:
         C_list_bin = []
         for k in range(N_bins):
